89de42e1e29f75d1f9258d054442441b595b624c
sy071500 <sy071500@naver.com>
2022-06-21 09:45:05 +0900

    React :: Dayjs

---
+++ date2/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import DayjsExample from './components/Dayjs/DayjsExample';
+
+function App() {
+  return (
+    <div className="App">
+      <DayjsExample/>
+    </div>
+  );
+}
+
+export default App;
