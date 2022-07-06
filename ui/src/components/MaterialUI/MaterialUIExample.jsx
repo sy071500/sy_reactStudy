cf085d92c44243f1468bf7c7a62b469441f0f195
sy071500 <sy071500@naver.com>
2022-07-06 10:26:43 +0900

    React :: Material UI

--- ui/src/components/MaterialUI/MaterialUIExample.jsx
+++ ui/src/components/MaterialUI/MaterialUIExample.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ButtonExample from './ButtonExample'
-import CenteredGrid from './CenteredGrid'
+import React from "react";
+import ButtonExample from "./ButtonExample";
+import CenteredGrid from "./CenteredGrid";
 
 export default function MaterialUIExample() {
   return (
     <div>
-      <CenteredGrid/>
-      <ButtonExample/>
+      <CenteredGrid />
+      <ButtonExample />
     </div>
-  )
+  );
 }
